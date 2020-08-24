b44f4c775c0a17b8f987c3ee76d13388f7655e19
Sridevies <30491104+Sridevies@users.noreply.github.com>
2020-08-24 13:06:58 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
     // var datesForDisable = ["25.12.2019", "26.12.2019", "27.12.2019"]
     // var meetingEvent = ["10/11/2019","11/11/2019","21/11/2019","22/11/2019","23/11/2019","24/11/2019"];
     // var birthdayEvent = ["25/11/2019","26/11/2019"]
+    $("#calendar").removeClass("hasDatepicker");
     var Events = ["3/8/2020","12/8/2020","3/9/2020","12/9/2020","21/9/2020"];
     $("#calendar").datepicker({
          format: "dd/mm/yyyy",
